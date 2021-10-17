84808ae88c3f9cdea154bf82f9b83aea306a0a78
ramiqayya <rami.qayya@gmail.com>
2021-10-17 08:53:27 +0300

    Ground
    ground is done

---
+++ Programing Theory Project/Assets/Scripts/EnemyMotion.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMotion : MonoBehaviour
+{
+    private float xRange;
+    private float zRange;
+    public float enemySpeed=10;
+    Rigidbody enemyRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = gameObject.GetComponent<Rigidbody>();
+        InvokeRepeating("EnemyRandomMotion", 1, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void EnemyRandomMotion()
+    {
+        xRange = Random.Range(-10, 10);
+        zRange = Random.Range(-10, 10);
+
+        enemyRb.AddForce(xRange, 0, zRange, ForceMode.Impulse);
+
+        //transform.Translate(Vector3.back*enemySpeed*xRange*Time.deltaTime);
+        //transform.Translate(Vector3.right* enemySpeed*xRange*Time.deltaTime);
+
+    }
+}
